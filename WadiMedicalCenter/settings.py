bdd6abf4660279a68c153600203820eadcb5ebc3
Daas33 <hatweekdaas@gmail.com>
2023-05-07 21:58:43 +0300

    test image 2s

--- WadiMedicalCenter/settings.py
+++ WadiMedicalCenter/settings.py
@@ -112,16 +112,19 @@ WSGI_APPLICATION = "WadiMedicalCenter.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#      "default": dj_database_url.parse(
-#         os.environ.get("DATABASE_URL")
-#      )
-#     #  {
-#     #     "ENGINE": "django.db.backends.sqlite3",
-#     #     "NAME": BASE_DIR / "db.sqlite3",
-#     # }
+# DATABASES = 
+# # {
+# #     'default': {
+# #         'ENGINE': 'django.db.backends.sqlite3',
+# #         'NAME': BASE_DIR / 'db.sqlite3',
+# #     }
+# # }
+#     #    {
+#     #  "default": dj_database_url.parse(
+#     #     os.environ.get("DATABASE_URL")
+#     #  )
 
-# }
+
 # To use Neon with Django, you have to create a Project on Neon and specify the project connection settings in your settings.py in the same way as for standalone Postgres.
 
 DATABASES = {
